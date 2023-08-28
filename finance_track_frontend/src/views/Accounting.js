4b68e0b30a3b6d97e1d3b8bfdb025bf82b409924
0xN1ck <alex97ander97@gmail.com>
2023-08-29 01:04:16 +0300

    Added resize for container to decrease width  of typeahead

--- finance_track_frontend/src/views/Accounting.js
+++ finance_track_frontend/src/views/Accounting.js
@@ -26,11 +26,11 @@ import {
   ListGroup,
   ListGroupItem,
   Button,
+  Col,
 } from "reactstrap";
 // core components
 import Header from "components/Headers/Header.js";
 import { useState, useEffect } from "react";
-// import Turnstone from 'turnstone'
 import { Typeahead } from "react-bootstrap-typeahead";
 import axios from "axios";
 
@@ -39,25 +39,8 @@ import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 const Accounting = () => {
 
-  const [listbox, setListbox] = useState({ data: [] });
-
-  // useEffect(() => {
-  //   axios.get('http://localhost:8000/api/worker/', {
-  //     headers: {
-  //       Authorization: `Bearer ${localStorage.getItem('token')}`
-  //     }
-  //   })
-  //     .then(response => {
-  //       setListbox({ data: response.data.map(item => item.username) });
-  //     })
-  //     .catch(error => {
-  //       console.log(error);
-  //     });
-  // }, []);
-
-  console.log(listbox);
-
   const [options, setOptions] = useState([]);
+  const [selectedOption, setSelectedOption] = useState([]);
 
   useEffect(() => {
     axios.get('http://localhost:8000/api/worker/', {
@@ -73,57 +56,34 @@ const Accounting = () => {
       });
   }, []);
 
-
-
-
-
-  // const listbox = {
-  //   displayField: 'characters',
-  //   data: async (query) => {
-  //     const res = await fetch(
-  //       `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${query}&apikey=${process.env.REACT_APP_MARVEL_APIKEY}`
-  //     )
-  //     const data = await res.json()
-  //     return data.data.results
-  //   },
-  //   searchType: 'startsWith',
-  // }
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+  }
 
   return (
     <>
       <Header />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
+
       {/* Page content */}
-      <Container className="mt--7" fluid>
-        {/* <Turnstone
-          cancelButton={true}
-          debounceWait={250}
-          id="search"
-          listbox={listbox}
-          listboxIsImmutable={true}
-          matchText={true}
-          maxItems={6}
-          name="search"
-          noItemsMessage="We found no places that match your search"
-          placeholder="Enter a city or airport"
-          typeahead={true}
-        /> */}
+      <div style={{ marginTop: "130px" }}>
+        {/* <Row>
+          <Col > */}
+            <Container className="mt--7 col-lg-6">
+              <Typeahead
+                clearButton
+                id="selections-example"
+                labelKey="name"
+                options={options}
+                onChange={handleChange}
+                placeholder="Выберите сотрудника..."
+              />
+            </Container>
+          {/* </Col>
+        </Row> */}
+      </div>
 
-        <Typeahead
-          clearButton
-          id="selections-example"
-          labelKey="name"
-          options={options}
-          placeholder="Выберите сотрудника..."
-        />
 
-        {/* Table */}
-      </Container>
+      {/* Table */}
     </>
   );
 };
